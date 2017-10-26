0f6952afaec202ff83069cbce17e3699a5da24ad
Rishi Patel <rishirocks421@gmail.com>
2017-10-26 00:47:06 -0400

    Log the god damn in

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
     }
     else{
         return(
-          <Log/>
+        <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
         )
     }
   }
