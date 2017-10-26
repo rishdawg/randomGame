b21666d3d6c1d16f9ca42f9811162436983cabbf
Rishi Patel <rishirocks421@gmail.com>
2017-10-26 00:33:04 -0400

    its login time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Loggedin from './components/Loggedin';
 import Log from './components/Log';
 
 const FB = window.FB;
@@ -17,18 +18,16 @@ class App extends Component {
     FB.getLoginStatus(this.setloginstatus.bind(this));
   }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{this.state.mode}</p>
-      </div>
-    );
+    if(this.state.mode === 'connected '){
+      return(
+            <Loggedin/>
+      );
+    }
+    else{
+        return(
+          <Log/>
+        )
+    }
   }
 }
 
